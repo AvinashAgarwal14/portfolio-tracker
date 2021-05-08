d6596560c660ef738ada5f7ffa3ccce0fc113f1e
Avinash Agarwal <agarwalavinash14@gmail.com>
2021-05-08 14:42:01 +0530

    portfolio add route added

--- index.js
+++ index.js
@@ -2,60 +2,12 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 require('./models/trade');
+require('./models/portfolio');
 mongoose.connect('mongodb://127.0.0.1/portfolio_tracker', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.json())
 require('./routes/tradeRoutes')(app);
-const Trades = mongoose.model('trades');
-app.get('/trade', async (req, res) => {
-    let trades = await Trades.find({});
-    res.send(trades);
-});
-
-app.post('/trade', async (req, res) => {
-    console.log(req.body);
-    const {securityName, type, quantity, price} = req.body;
-    const trade = new Trades({securityName, type, quantity, price});
-    try {
-        await trade.save()
-        res.sendStatus(200);
-    } catch (err) {
-        res.status(422).send(err);
-    }
-});
-
-app.put('/trade/:tradeId', async (req, res) => {
-    const tradeId = req.params.tradeId;
-    const {securityName, type, quantity, price} = req.body;
-    try {
-        await Trades.updateOne(
-            {
-                _id: tradeId,
-            },
-            { 
-                securityName, type, quantity, price
-            }
-        )
-        res.sendStatus(200);
-    } catch (err) {
-        res.status(422).send(err);
-    }
-});
-
-app.delete('/trade/:tradeId', async (req, res) => {
-    const tradeId = req.params.tradeId;
-    const {securityName, type, quantity, price} = req.body;
-    try {
-        await Trades.deleteOne(
-            {
-                _id: tradeId,
-            }
-        )
-        res.sendStatus(200);
-    } catch (err) {
-        res.status(422).send(err);
-    }
-});
+require('./routes/portfolioRoutes')(app);
 
 app.listen(5000, (req, res) => {
     console.log("Server Started!");
--- routes/tradeRoutes.js
+++ routes/tradeRoutes.js
@@ -1,19 +1,58 @@
 const mongoose = require('mongoose');
 const Trades = mongoose.model('trades');
+const Portfolio = mongoose.model('portfolio');
+
 module.exports  = (app) => {
     app.get('/trade', async (req, res) => {
-        let trades = await Trades.find({});
-        res.send(trades);
+        try {
+            let trades = await Trades.find({});
+            res.send(trades);
+        } catch (err) {
+            res.status(400).send({error: err.message});
+        }
     });
 
     app.post('/trade', async (req, res) => {
+        let trade;
+        let updatedPortfolio;
         const {securityName, type, quantity, price} = req.body;
-        const trade = new Trades({securityName, type, quantity, price});
+        const prevPortfolio = await Portfolio.find({securityName: securityName});
         try {
-            await trade.save()
+            if(prevPortfolio.length){
+                let portfolioQuantity = prevPortfolio[0].quantity;
+                let portfolioAvgPrice = prevPortfolio[0].avgPrice;
+                if(type === "SELL" && quantity > portfolioQuantity) {
+                    res.status(400).send({error: "Insufficeint Quanity"});
+                    return;
+                }
+                if(type === "BUY") {
+                    portfolioAvgPrice = (portfolioAvgPrice*portfolioQuantity + price*quantity)/(portfolioQuantity+quantity);
+                    portfolioQuantity += quantity;
+                }
+                if(type == "SELL") {
+                    portfolioQuantity -= quantity;
+                }
+                trade = await Trades.create({securityName, type, quantity, price});
+                updatedPortfolio = await Portfolio.updateOne(
+                    {
+                        securityName: securityName
+                    },
+                    {
+                        quantity: portfolioQuantity,
+                        avgPrice: portfolioAvgPrice
+                    }
+                );
+            } else {
+                if(type == "SELL") {
+                    res.status(400).send({message: "Insufficeint Quanity"});
+                    return;
+                }
+                trade = await Trades.create({securityName, type, quantity, price});
+                updatedPortfolio = await Portfolio.create({securityName, quantity, avgPrice: price});
+            }
             res.sendStatus(200);
         } catch (err) {
-            res.status(422).send(err);
+            res.status(400).send({error: err.message});
         }
     });
 
@@ -31,7 +70,7 @@ module.exports  = (app) => {
             )
             res.sendStatus(200);
         } catch (err) {
-            res.status(422).send(err);
+            res.status(400).send({error: err.message});
         }
     });
 
@@ -45,7 +84,7 @@ module.exports  = (app) => {
             )
             res.sendStatus(200);
         } catch (err) {
-            res.status(422).send(err);
+            res.status(400).send({error: err.message});
         }
     });
 }
